4eb6c978ebc3a8fcad31e02eb21f299f6e9c5006
Łukasz Klich <klich.lukasz@gmail.com>
2015-01-20 22:28:50 +0100

    Sending metrics to graphite. I hope

--- src/clogrid/core/handler.clj
+++ src/clogrid/core/handler.clj
@@ -7,13 +7,23 @@
             [hiccup.middleware :refer [wrap-base-url]]
             [metrics.ring.expose :refer [expose-metrics-as-json]]
             [metrics.ring.instrument :refer [instrument]]
+            [metrics.reporters.graphite :as graphite]
             [clogrid.schedule.client :as schedule]
             [clogrid.core.grid :as grid]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json])
+  (:import (com.codahale.metrics MetricFilter)
+           (java.util.concurrent TimeUnit)))
 
+(def GR (graphite/reporter {:host "localhost"
+                            :port 2003
+                            :prefix "my-api.common.prefix"
+                            :rate-unit TimeUnit/SECONDS
+                            :duration-unit TimeUnit/MILLISECONDS
+                            :filter MetricFilter/ALL}))
+(graphite/start GR 1)
 
 (defn channel-info [region channel]
-  (schedule/channel region channel))
+  (schedule/get-channel region channel))
 
 (defroutes app-routes
            (GET "/:region/channels.json" [region :as {query-params :query-params}]
